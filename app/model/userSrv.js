6f278e5cd8ec6ac38c61ed286981de9596eb4825
nirch <nirh2@yahoo.com>
2019-02-28 11:22:22 +0200

    login logic now connected to users.json

--- app/model/userSrv.js
+++ app/model/userSrv.js
@@ -1,5 +1,5 @@
 
-app.factory("userSrv", function($http, $q) {
+app.factory("userSrv", function($http, $q, $log) {
 
     var activeUser = null;
 
@@ -13,12 +13,30 @@ app.factory("userSrv", function($http, $q) {
     function login(email, pwd) {
         var async = $q.defer();
 
-        if (email === "nir@nir.com" && pwd === "123") {
-            activeUser = new User({id: "1", fname: "Nir", lname: "Channes", email:"nir@nir.com"});
-            async.resolve(activeUser);
-        } else {
-            async.reject();
-        }
+        $http.get("app/model/data/users.json").then(function(response) {
+            var users = response.data;
+            for (var i = 0; i < users.length; i++) {
+                if (users[i].email === email && users[i].pwd === pwd) {
+                    activeUser = new User(users[i]);
+                    async.resolve(activeUser);
+
+                }
+            }
+
+            if (!activeUser) {
+                async.reject("");
+            }
+        }, function(error) {
+            $log.error(error);
+            async.reject(error);
+        })
+
+        // if (email === "nir@nir.com" && pwd === "123") {
+        //     activeUser = new User({id: "1", fname: "Nir", lname: "Channes", email:"nir@nir.com"});
+        //     async.resolve(activeUser);
+        // } else {
+        //     async.reject();
+        // }
 
         return async.promise;
     }
