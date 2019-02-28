25d49cb1e5a42e59faf7f1dbe0d22e6e5f104599
nirch <nirh2@yahoo.com>
2019-02-28 11:58:15 +0200

    recipe gallery with cards - data hardcoded

--- app/recipes/recipesCtrl.js
+++ app/recipes/recipesCtrl.js
@@ -8,4 +8,26 @@ app.controller("recipesCtrl", function($scope, userSrv, $location) {
 
     $scope.activeUser = userSrv.getActiveUser();
 
+
+    $scope.recipes = [
+        {
+            "id": "1",
+            "name": "Shakshuka",
+            "description": "Eggs with tomatos",
+            "imgUrl": "https://static01.nyt.com/images/2014/09/02/dining/shakshuka/shakshuka-superJumbo-v2.jpg",
+            "ingredients": "Eggs, Tomato, Onion",
+            "steps": "Put eggs and then tomato",
+            "duration": 15
+        },
+        {
+            "id": "2",
+            "name": "Shawarma",
+            "description": "Turkey in pita",
+            "imgUrl": "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2014/2/28/0/ZB0505H_Chicken-Shawarma-with-Tomato-Cucumber-Relish-and-Tahini-Sauce_s4x3.jpg.rend.hgtvcom.616.462.suffix/1485881272706.jpeg",
+            "ingredients": "Eggs, Tomato, Onion",
+            "steps": "Put eggs and then tomato",
+            "duration": 15
+        }
+    ]
+
 })
