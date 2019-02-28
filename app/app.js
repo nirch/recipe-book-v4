7227d1224bf9c62b1689788a15fae2c3686b882c
nirch <nirh2@yahoo.com>
2019-02-28 09:45:30 +0200

    adding ngRoute to project + initial home html

--- app/app.js
+++ app/app.js
@@ -1,2 +1,20 @@
 
-var app = angular.module("recipeBook", []);
+var app = angular.module("recipeBook", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider.when("/", {
+        templateUrl: "app/home/home.html"
+    }).when("/login", {
+
+    }).when("/signup", {
+
+    }).when("/recipes", {
+
+    }).when("/recipe/:recipeId", {
+
+    }).when("/new" , {
+
+    }).otherwise({
+        redirectTo: "/"
+    })
+})
